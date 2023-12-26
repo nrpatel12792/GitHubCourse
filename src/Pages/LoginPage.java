05f523c115fb31e826aed5490ea9820ad44bb862
nrpatel12792 <nrpatel12792@gmail.com>
2023-12-26 14:11:42 -0500

    added log out steps

--- src/Pages/LoginPage.java
+++ src/Pages/LoginPage.java
@@ -4,5 +4,6 @@ public class LoginPage {
 	
 	public static void main(String args[]) {
 	 System.out.println("Login Page");
+	 System.out.println("Log out");
 	}
 }
--- src/Pages/SearchPage.java
+++ src/Pages/SearchPage.java
@@ -4,5 +4,6 @@ public class SearchPage {
 
 	public static void main(String args[]) {
 		 System.out.println("Search Page");
+		 System.out.println("Log out");
 		}
 }
