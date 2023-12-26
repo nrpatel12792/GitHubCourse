a3e5ac1f434d32d207363333923fd25f4550f6d3
nrpatel12792 <nrpatel12792@gmail.com>
2023-12-26 13:53:11 -0500

    first comment

---
+++ src/Tests/HomePageTest.java
@@ -0,0 +1,9 @@
+package Tests;
+
+public class HomePageTest {
+	
+	public void homePage() {
+		System.out.println("Home page teest - 1");
+	}
+
+}
---
+++ src/Tests/LoginPageTest.java
@@ -0,0 +1,9 @@
+package Tests;
+
+public class LoginPageTest {
+	
+	public void loginPage() {
+		System.out.println("Login page teest - 1");
+	}
+
+}
